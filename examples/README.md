87bad8dc711f2e903a79b7963653d7ccada4bb0a
GeekDream-x <inace0302@gmail.com>
2021-11-08 12:43:53 +0800

    add examples

---
+++ examples/README.md
@@ -0,0 +1,88 @@
+## 使用方法
+
+用户可以基于已发布的上述预训练模型进行下游任务精调及zero-shot测试。
+
+### 测试环境
+- numpy : 1.21.2
+- python : 3.7.10
+- pytorch : 1.7.1
+- scikit-learn : 0.24.2
+- transformers : 3.1.0
+
+### Finetune
+
+本例中，我们使用 `CINO-large` 模型在维基分类任务中文数据上进行精调，相关步骤如下。假设，  
+- `project-dir`：工作根目录，可按实际情况设置。
+- `data-dir`：数据目录，本例为 `${project-dir}/data/`。
+- `model_pretrain_dir`：预训练模型目录，本例为 `${project-dir}/model/`。
+- `model_save_dir`：精调最优模型参数存储目录，本例为 `${project-dir}/saved_models/`。
+- `best_model_save_name`：精调最优模型参数文件名，本例为 `best_cino.pth`。
+
+#### 第一步：模型准备
+在[模型下载](#模型下载)章节中，下载`CINO-large`模型，并解压至`${project-dir}/model/。
+该目录下应包含`pytorch_model.bin`，`sentencepiece.bpe.model`，`config.json`，共计3个文件。
+
+#### 第二步：数据准备
+参照[少数民族语言分类数据集](#少数民族语言分类数据集)章节中的说明，下载Wiki-Chinese-Minority（WCM）数据集中中文数据到`${data-dir}`，并保持原文件名。即`train.txt`、`dev.txt`和`test.txt`。
+
+#### 第三步：运行训练命令
+```shell
+python run_finetune.py --params cino-params.json
+```
+`params`是一个JSON词典，在本例中的`cino-params.json`包含了精调相关参数，例如：
+```json
+{
+    "learning_rate":5e-6,
+    "epoch":5,
+    "gradient_acc":4,
+    "batch_size":16,
+    "max_len":512,
+    "weight_decay":1e-4,
+    "warmup_rate":0.1,
+    "data_dir":"data/",
+    "model_pretrain_dir":"model/", 
+    "model_save_dir":"saved_models/",
+    "best_model_save_name":"best_cino.pth",
+    "class_names":["艺术", "地理", "历史", "自然", "自然科学", "人物", "技术", "教育", "经济", "健康"] 
+}
+```
+
+运行完毕后，精调过程的日志信息和模型测试结果可在`${project-dir}/log/cino-ft.log`中查看。
+
+
+
+### Zero-Shot
+
+本例中，我们使用 `CINO-large` 模型在维基分类任务少数民族语言数据上进行zero-shot测试，相关步骤如下。假设，  
+- `project-dir`：工作根目录，可按实际情况设置。
+- `data-dir`：数据目录，本例为 `${project-dir}/data/`。
+- `model_pretrain_dir`：预训练模型目录，本例为 `${project-dir}/model/`。
+- `model_finetune_params`：用于zero-shot的模型参数路径，本例为 `${project-dir}/model/best_cino.pth`。
+
+#### 第一步：模型准备
+在[模型下载](#模型下载)章节中，下载`CINO-large`模型，并解压至`${project-dir}/model/。
+该目录下应包含`pytorch_model.bin`，`sentencepiece.bpe.model`，`config.json`，共计3个文件。
+
+将finetune阶段保存的最优模型参数文件放于`${project-dir}/model/`。
+
+#### 第二步：数据准备
+参照[少数民族语言分类数据集](#少数民族语言分类数据集)章节中的说明，下载Wiki-Chinese-Minority（WCM）数据集中少数民族语言数据到`${data-dir}`，并保持原文件名。即`bo.txt`、`kk.txt`、`ko.txt`、`mn.txt`、`ug.txt`和`yue.txt`。
+
+#### 第三步：运行训练命令
+```shell
+python run_zeroshot.py --params cino-params.json
+```
+`params`是一个JSON词典，在本例中的`cino-params.json`包含了zero-shot相关参数，例如：
+```json
+{
+    "batch_size":16,
+    "max_len":512,
+    "model_pretrain_dir":"model/",
+    "model_finetune_params":"model/best_cino.pth",
+    "data_dir":"data/",
+    "class_names":["艺术", "地理", "历史", "自然", "自然科学", "人物", "技术", "教育", "经济", "健康"] 
+}
+```
+
+运行完毕后，zero-shot测试结果可在`${project-dir}/log/cino-zs.log`中查看。
+
---
+++ examples/zeroshot/run_zeroshot.py
@@ -0,0 +1,138 @@
+import os
+import random
+import json, time
+import logging
+import argparse
+import numpy as np
+from glob import glob
+import torch
+import torch.nn as nn
+from torch.nn.modules import padding
+import torch.optim as optim
+import torch.nn.functional as F
+from sklearn.metrics import f1_score, classification_report
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from transformers import XLMRobertaConfig, XLMRobertaModel, XLMRobertaTokenizer, AdamW, get_cosine_schedule_with_warmup
+
+logging.basicConfig(filename='log/cino_zs.log', filemode="w",
+                    format="%(asctime)s %(name)s:%(levelname)s:%(message)s",
+                    datefmt="%d-%m-%Y %H:%M:%S",
+                    level=logging.DEBUG)
+logger = logging.getLogger("CINO Logger")
+
+
+def load(json_file):
+
+    if json_file.endswith(".json"):
+        with open(json_file, 'r') as f:
+            return json.load(f)
+    else:
+        return json.loads(json_file)
+
+
+class CINO_Model(nn.Module):
+
+    def __init__(self, cino_path, class_num):
+        
+        super(CINO_Model, self).__init__()
+        self.config = XLMRobertaConfig.from_pretrained(cino_path)
+        self.cino = XLMRobertaModel.from_pretrained(cino_path)
+        for param in self.cino.parameters():
+            param.requires_grad = True
+        self.fc = nn.Linear(self.config.hidden_size, class_num)
+
+    def forward(self, input_ids, attention_mask):
+
+        output = self.cino(input_ids, attention_mask)[1]
+        logit = self.fc(output)
+        return logit
+
+class CINO_ZS_Configer():
+
+    def __init__(self, params_dict: dict):
+        
+        super().__init__()
+        
+        self.batch_size = params_dict['batch_size']
+        self.max_len = params_dict['max_len']
+        self.model_pretrain_dir = params_dict['model_pretrain_dir']
+        self.model_finetune_params = params_dict['model_finetune_params']
+        self.class_names = params_dict['class_names']
+        self.data_dir = params_dict['data_dir']
+
+class CINO_ZeroShoter():
+
+    def __init__(self, config: CINO_ZS_Configer):
+
+        super().__init__()
+
+        self.config = config
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.tokenizer = XLMRobertaTokenizer.from_pretrained(self.config.model_pretrain_dir)
+
+    def dataset(self, data_path):
+
+        input_ids, attention_masks, labels = [], [], []
+        with open(data_path, encoding='utf-8') as f:
+            for i, line in enumerate(f):
+                text, label = line.strip().split('\t')
+                encode_dict = self.tokenizer.encode_plus(text=text, \
+                                                        max_length=self.config.max_len, \
+                                                        padding='max_length', \
+                                                        truncation=True)
+                input_ids.append(encode_dict['input_ids'])
+                attention_masks.append(encode_dict['attention_mask'])
+                labels.append(int(label))
+        
+        return torch.tensor(input_ids), torch.tensor(attention_masks), torch.tensor(labels)
+    
+    def data_loader(self, input_ids, attention_masks, labels):
+
+        data = TensorDataset(input_ids, attention_masks, labels)
+        loader = DataLoader(data, batch_size=self.config.batch_size, shuffle=True)
+
+        return loader
+
+    def predict(self, model, data_loader):
+
+        model.eval()
+        test_pred, test_true = [], []
+        with torch.no_grad():
+            for idx, (ids, att, y) in enumerate(data_loader):
+                y_pred = model(ids.to(self.device), att.to(self.device))
+                y_pred = torch.argmax(y_pred, dim=1).detach().cpu().numpy().tolist()
+                test_pred.extend(y_pred)
+
+                y_temp = y.squeeze().cpu().numpy().tolist()
+                test_true.extend([y_temp] if type(y_temp) == int else y_temp)
+            return test_true, test_pred
+
+
+    def zeroshot(self):
+
+        model = CINO_Model(self.config.model_pretrain_dir, class_num=len(self.config.class_names)).to(self.device)
+        model.load_state_dict(torch.load(self.config.model_finetune_params))
+
+        for data_file in glob(f"{self.config.data_dir}*.txt"):
+            lan = data_file.split('/')[-1][:-4]
+            logger.info("\tCurrent Language : " + lan)
+            test_loader = self.data_loader(*self.dataset(data_file))
+
+            test_true, test_pred = self.predict(model, test_loader)
+            logger.info('\n\n' + classification_report(test_true, test_pred,
+                                                target_names=self.config.class_names,
+                                                digits=6))
+
+def main():
+
+    parser = argparse.ArgumentParser(description="CINO-Argparser")
+    parser.add_argument("--params", default={}, help="JSON dict of model hyperparameters.")   
+    args = parser.parse_args()
+
+    config = CINO_ZS_Configer(load(args.hparams))
+
+    trainer = CINO_ZeroShoter(config)
+    trainer.zeroshot()
+
+if __name__ == "__main__":
+    main()
